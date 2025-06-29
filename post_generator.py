fa8e1264d3a65397cc1a1494a4cdc7fc62e8cdb0
Priya Gupta <priyagupta052k@gmail.com>
2025-06-29 23:42:12 +0530

    Add Prompt

--- post_generator.py
+++ post_generator.py
@@ -5,3 +5,20 @@ from dotenv import load_dotenv
 load_dotenv()
 openai_api_key = os.getenv("Open_api_key")
 
+# generate post
+ 
+def generate_post(data):
+    questions_format = ""
+    for i,q in enumerate(data["questions"]):
+        questions_format+=f"Problem{i+1}:{q}\n"
+
+promt = f"""
+You are helping a developer to write a LinkedIn post for Day{data['day']} of #100DaysOfDSAChallenge.
+- Start with: Day {data ['day']} of 100 | #100DaysOfDSAChallenge.
+- Topic : {data['topic']}
+- Problem Solved : {question_foramt}
+- Based on these learnings: {data['learning']}
+Write 2 bullet points starting with ~ to summarize key learnings.
+
+Add 8–10 relevant hashtags including #100DaysOfDSAChallenge, #DSA, #Java, #ProblemSolving, #CodingChallenge.
+"""
